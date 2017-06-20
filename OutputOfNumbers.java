a158ccbb2cc42c04c04c394f91423b539a6914b1
ZhilinskayaAnastasia <a1680898@gmail.com>
2017-06-20 23:35:44 +0300

    Update OutputOfNumbers.java

--- OutputOfNumbers.java
+++ OutputOfNumbers.java
@@ -1,5 +1,5 @@
 public class OutputOfNumbers {
-	public static void main (String[] args) {
+    public static void main (String[] args) {
 		for (int k = 0; k <= 100; k++) {
 			if (k % 3 == 0) {
 				System.out.println("3*" + k/3);
