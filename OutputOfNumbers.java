2cc40682af1158c1e90ed68617c1f2512a96517b
ZhilinskayaAnastasia <a1680898@gmail.com>
2017-06-20 23:40:18 +0300

    Update OutputOfNumbers.java

--- OutputOfNumbers.java
+++ OutputOfNumbers.java
@@ -1,11 +1,11 @@
 public class OutputOfNumbers {
     public static void main (String[] args) {
-		for (int k = 0; k <= 100; k++) {
-			if (k % 3 == 0) {
-				System.out.println("3*" + k/3);
-			} else {
-				System.out.println(k);
-			}
-		}
-	}
+        for (int k = 0; k <= 100; k++) {
+            if (k % 3 == 0) {
+                System.out.println("3*" + k/3);
+            } else {
+                System.out.println(k);
+            }
+        }
+    }
 }
