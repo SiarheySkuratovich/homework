44d833c82c4f65f9b9b3d660859b8d9682e19dfc
ZhilinskayaAnastasia <a1680898@gmail.com>
2017-06-21 13:12:06 +0300

    Update OutputOfNumbers.java

--- OutputOfNumbers.java
+++ OutputOfNumbers.java
@@ -1,15 +1,15 @@
-public class OutputOfNumbersUpgrd {
-    public static void main (String[] args) {  
-        for (int k = 0; k <= 100; k++) { 
-            if (k % 3 == 0 || k % 5 == 0) { 
-                if (k % 3 == 0 && k % 5 != 0) 
-                    System.out.println("Tutti"); 
-                if (k % 5 == 0 && k % 3 != 0) 
-                    System.out.println("Frutti"); 
-                if (k % 3 == 0 && k % 5 == 0) 
-                    System.out.println("Tutti-Frutti"); 
-                } else 
-                    System.out.println(k); 
-                }
-            }
+public class OutputOfNumbers {
+  public static void main (String[] args) {  
+    for (int k = 0; k <= 100; k++) { 
+      if (k % 3 == 0 || k % 5 == 0) { 
+        if (k % 3 == 0 && k % 5 != 0) 
+          System.out.println("Tutti"); 
+        if (k % 5 == 0 && k % 3 != 0) 
+          System.out.println("Frutti"); 
+        if (k % 3 == 0 && k % 5 == 0) 
+          System.out.println("Tutti-Frutti"); 
+        } else 
+          System.out.println(k); 
         }
+      }
+    }
