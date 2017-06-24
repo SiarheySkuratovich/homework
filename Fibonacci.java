b267c0712984b7c1e546c724e26eaf2b5bce3b01
ZhilinskayaAnastasia <a1680898@gmail.com>
2017-06-24 14:43:00 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,38 +1,36 @@
 import java.util.Scanner;
 
 public class Fibonacci {
-  public static void main (String[] args) {
-    String text;
-    int count = 0;
-    Scanner number = new Scanner(System.in);
-    text = number.next();
-    try {
-      count = Integer.parseInt(text); 
-    } catch(NumberFormatException e) {
-        while(!number.hasNextInt()) { 
-        System.out.println("It is not a count"); 
-        text=number.next(); 
-      }
-    } 
-    
-    final int max = 2147483647;
-    boolean success = false;
-    int sum;
-    int next = 0;
-    int previous = 1;
-      while (next < max / 2) {
-        sum = next + previous;
-        next = previous;
-        previous = sum;
-          if (count == sum) {
-            success = true;
-            System.out.println("Your number is count from Fibonacci");
-            break; 
-          }       
-        } if (success == false) {
-            System.out.println("Your number is not count from Fibonacci");
-        }
-      }
-    }
-  
-    
+  private String NotNumber;
+  private int count;
+public Fibonacci() {
+  Scanner number = new Scanner(System.in);
+    while (!number.hasNextInt()) {
+      System.out.println("It is not a number");
+      NotNumber = number.next();
+  } if(number.hasNextInt()) {
+      NotNumber = number.next();
+      count = Integer.parseInt(NotNumber); }
+  }
+public void fibonacci() {
+  boolean success = false;
+  int sum;
+  int next = 0;
+  int previous = 1;
+  while (next < count) {
+    sum = next + previous;
+    next = previous;
+    previous = sum;
+  if (count == sum) {
+    success = true;
+    System.out.println("Your number is count from Fibonacci");
+    break;
+    }       
+} if (success == false) {
+    System.out.println("It is not a number from Fibonacci");
+  }  
+}
+public static void main(String[] args) {
+  Fibonacci f = new Fibonacci();
+  f.fibonacci(); }
+}
