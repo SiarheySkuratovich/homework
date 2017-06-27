014352accbb6595fd0996ec662df6cca04b18d19
ZhilinskayaAnastasia <a1680898@gmail.com>
2017-06-27 23:54:07 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -11,68 +11,68 @@ public class Sequence {
   private ArrayList<Integer> transformation;
   private Scanner Input; 
   
-/** 
- * To re-query the data. 
- */
-public boolean again() {
-String yes = "y";
-Input = new Scanner(System.in);
-System.out.println("Wish you continue, so input y? ");
-answer = Input.next();
-return answer.equals(yes);
-}
+  /** 
+   * To re-query the data. 
+   */
+  public boolean again() {
+    String yes = "y";
+    Input = new Scanner(System.in);
+    System.out.println("Wish you continue, so input y? ");
+    answer = Input.next();
+    return answer.equals(yes);
+  }
+  
+  /** 
+   * To verify input data. 
+   */
+  public void input() {
+    while (!number.hasNextInt()) {
+      System.out.println("It is not a count");
+      notNumber = Input.next();
+      } 
+    if (Input.hasNextInt()) {
+      notNumber = Input.next();
+      count = Integer.parseInt(text);
+      }
+    }
 
-/** 
- * To verify input data. 
- */
-public void input() {
-  while (!number.hasNextInt()) {
-    System.out.println("It is not a count");
-    notNumber = Input.next();
-    } 
-  if (Input.hasNextInt()) {
-    notNumber = Input.next();
-    count = Integer.parseInt(text);
+  /** 
+   * For an array with an undefined number of values. 
+   */
+  public Sequence() {
+    transformation = new ArrayList<Integer>();
+    Input = new Scanner(System.in);
+  }
+
+  public void toArray() {
+    transformation.add(number);
+  }
+
+  /** 
+   * Check for affiliation with positive sequence. 
+   */
+  public void check() {
+    boolean b = true;
+    for (int j = 0; j < (transformation.size() - 1); j++) {
+    if (transformation.get(j) > transformation.get(j + 1)) {
+      b = false;
+      System.out.println("Not all elements is more then previous");
+      break;
+      }
+    }
+    if (b == true) {
+      System.out.println("All elements is more then previous  or equal");
+      }
+    }
+
+  public static void main (String[] args) {
+    Sequence S = new Sequence(); 
+    boolean ans = false;
+    while (S.again()) {
+      S.input();
+      S.toArray();
+      ans = S.again();
+    }
+    S.check();
     }
   }
-
-/** 
- * For an array with an undefined number of values. 
- */
-public Sequence() {
-  transformation = new ArrayList<Integer>();
-  Input = new Scanner(System.in);
-}
-
-public void toArray() {
-  transformation.add(number);
-}
-
-/** 
- * Check for affiliation with positive sequence. 
- */
-public void check() {
-  boolean b = true;
-  for (int j = 0; j < (transformation.size() - 1); j++) {
-  if (transformation.get(j) > transformation.get(j + 1)) {
-    b = false;
-    System.out.println("Not all elements is more then previous");
-    break;
-    }
-  }
-  if (b == true) {
-    System.out.println("All elements is more then previous  or equal");
-    }
-  }
-
-public static void main (String[] args) {
-  Sequence S = new Sequence(); 
-  boolean ans = false;
-  while (S.again()) {
-    S.input();
-    S.toArray();
-    ans = S.again();
-  }
-  S.check();
-  }
-}
